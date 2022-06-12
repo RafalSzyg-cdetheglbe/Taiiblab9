444bdc360ed41209687793e39f93a1e2fdd5dfcb
Rafekpro <72563938+Rafekpro@users.noreply.github.com>
2022-06-12 22:01:02 +0200

    Lab9

---
+++ WebAPI/Models/Database.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Models
+{
+    public class Database : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(@"Data Source=C:\Users\Rafał\Desktop\taiib-lab-main\WebAPI\database.db");
+    }
+}
